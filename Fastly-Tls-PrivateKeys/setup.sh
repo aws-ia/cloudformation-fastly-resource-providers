6a2b2cf4c5a20c8a68e8d6fb38339dc821e690b8
Rajesh Duraisamy <rajdnp@amazon.com>
2023-03-21 10:04:54 -0700

    Adding Fastly TLS to CICD

---
+++ Fastly-Tls-PrivateKeys/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script runs in the buildspec before testing
+#
+# It relies on environment variables to be set to replace placeholder
+# values in `example-inputs`.
+#
+#   FASTLY_SERVICE_ID
+#   FASTLY_VERSION_ID
+#   Example:
+#
+#    GITHUB_ORG=ericzbeard-aws-cep-testing GITHUB_REPO=test-stable-1 AWS_PROFILE=ezbeard-cep GITHUB_SHA=6907b5086cfdc46381af05d94c2e41ac6812a962 ./setup.sh
+
+mkdir -p inputs
+
+cat example-inputs/inputs_1_create.json | sed "s/FASTLY_PRIVATE_KEY/${FASTLY_PRIVATE_KEY}/g" > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json | sed "s/FASTLY_PRIVATE_KEY/${FASTLY_PRIVATE_KEY}/g" > inputs/inputs_1_update.json
+cp example-inputs/inputs_1_invalid.json inputs/
+cat test/integ.yml | sed "s/FASTLY_PRIVATE_KEY/${FASTLY_PRIVATE_KEY}/g" > test/integ-unique.yml
+
+python3 ../get_type_configuration.py
