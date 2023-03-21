f76c44fb0adbf57e8564f5ab321f679ac6cfdbbe
Rajesh Duraisamy <rajdnp@amazon.com>
2023-03-21 10:47:50 -0700

    Adding Fastly TLS to CICD

--- Fastly-Tls-PrivateKeys/setup.sh
+++ Fastly-Tls-PrivateKeys/setup.sh
@@ -13,9 +13,9 @@
 
 mkdir -p inputs
 
-cat example-inputs/inputs_1_create.json | sed "s/FASTLY_PRIVATE_KEY/${FASTLY_PRIVATE_KEY}/g" > inputs/inputs_1_create.json
-cat example-inputs/inputs_1_update.json | sed "s/FASTLY_PRIVATE_KEY/${FASTLY_PRIVATE_KEY}/g" > inputs/inputs_1_update.json
+cat example-inputs/inputs_1_create.json  > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json > inputs/inputs_1_update.json
 cp example-inputs/inputs_1_invalid.json inputs/
-cat test/integ.yml | sed "s/FASTLY_PRIVATE_KEY/${FASTLY_PRIVATE_KEY}/g" > test/integ-unique.yml
+cat test/integ.yml > test/integ-unique.yml
 
 python3 ../get_type_configuration.py
