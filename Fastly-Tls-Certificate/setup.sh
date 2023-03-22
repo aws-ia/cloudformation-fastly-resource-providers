d971bc612fa009775c4b04b23ba44f30bc58389b
Rajesh Duraisamy <rajdnp@amazon.com>
2023-03-22 13:14:46 -0700

    Adding Fastly TLS to CICD - Fixing Review Comments

--- Fastly-Tls-Certificate/setup.sh
+++ Fastly-Tls-Certificate/setup.sh
@@ -5,12 +5,6 @@
 # It relies on environment variables to be set to replace placeholder
 # values in `example-inputs`.
 #
-#   FASTLY_SERVICE_ID
-#   FASTLY_VERSION_ID
-#   Example:
-#
-#    GITHUB_ORG=ericzbeard-aws-cep-testing GITHUB_REPO=test-stable-1 AWS_PROFILE=ezbeard-cep GITHUB_SHA=6907b5086cfdc46381af05d94c2e41ac6812a962 ./setup.sh
-
 mkdir -p inputs
 
 cat example-inputs/inputs_1_create.json  > inputs/inputs_1_create.json
