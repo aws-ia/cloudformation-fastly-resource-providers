0d7121e6de2ed65b239c9a5cef9bc96550056868
Rajesh Duraisamy <rajdnp@amazon.com>
2023-03-23 05:23:19 -0700

    Adding Fastly TLS to CICD

--- get_type_configuration.py
+++ get_type_configuration.py
@@ -10,7 +10,7 @@ def get_secret():
     "Get the secret from secrets manager"
 
     secret_name = "cep-fastly-type-configuration"
-    region_name = "us-east-1"
+    region_name = "us-west-2"
     session = boto3.session.Session()
     client = session.client(
         service_name="secretsmanager",
